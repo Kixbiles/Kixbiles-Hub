a8a7ac097273581d4a922ffe17036132bf283293
Kixbiles <103227978+Kixbiles@users.noreply.github.com>
2022-04-08 12:07:45 -0400

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,29 +1,65 @@
 
+local ScreenGui = Instance.new("ScreenGui")
 local Main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local UIGradient = Instance.new("UIGradient")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_2 = Instance.new("UICorner")
+local Hoverboard = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
 local SwordLord = Instance.new("TextButton")
-local SpinningSword = Instance.new("TextButton")
-local FlingSpinner = Instance.new("TextButton")
-local Hoverboard = Instance.new("TextLabel")
-local FlingSpinner_2 = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local SpinnerSword = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local RevovlerSword = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
 
---Properties:
 
 ScreenGui.Parent = game.CoreGui
 
 Main.Name = "Main"
-Main.Parent = game.StarterGui.ScreenGui
-Main.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-Main.BorderColor3 = Color3.fromRGB(40, 40, 40)
-Main.Position = UDim2.new(0.228310511, 0, 0.310139149, 0)
-Main.Size = UDim2.new(0, 393, 0, 190)
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Main.Position = UDim2.new(0.0954712331, 0, 0.230616301, 0)
+Main.Size = UDim2.new(0, 381, 0, 176)
 Main.Active = true
 Main.Draggable = true
+
+UICorner.Parent = Main
+
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(79, 117, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(28, 42, 93))}
+UIGradient.Parent = Main
+
+TextLabel.Parent = Main
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Size = UDim2.new(0, 381, 0, 28)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Jays Sword Gui | Remastered"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
+UICorner_2.Parent = TextLabel
+
+Hoverboard.Name = "Hoverboard"
+Hoverboard.Parent = Main
+Hoverboard.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Hoverboard.Position = UDim2.new(0.0787401572, 0, 0.261363626, 0)
+Hoverboard.Size = UDim2.new(0, 129, 0, 42)
+Hoverboard.Font = Enum.Font.SourceSans
+Hoverboard.Text = "Hoverboard (BETA)"
+Hoverboard.TextColor3 = Color3.fromRGB(0, 0, 0)
+Hoverboard.TextSize = 14.000
+Hoverboard.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Kixbiles/Kixbiles-Hub/main/Hoverboard.lua'))()
+end)
+
+UICorner_3.Parent = Hoverboard
 
 SwordLord.Name = "Sword Lord"
 SwordLord.Parent = Main
 SwordLord.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-SwordLord.Position = UDim2.new(0.0279898215, 0, 0.294736832, 0)
-SwordLord.Size = UDim2.new(0, 133, 0, 39)
+SwordLord.Position = UDim2.new(0.0787401572, 0, 0.625, 0)
+SwordLord.Size = UDim2.new(0, 129, 0, 49)
 SwordLord.Font = Enum.Font.SourceSans
 SwordLord.Text = "Sword Lord"
 SwordLord.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -32,51 +68,35 @@ SwordLord.MouseButton1Down:connect(function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Kixbiles/Kixbiles-Hub/main/sworduser.lua'))()
 end)
 
-SpinningSword.Name = "Spinning Sword"
-SpinningSword.Parent = Main
-SpinningSword.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-SpinningSword.Position = UDim2.new(0.0279898215, 0, 0.573684216, 0)
-SpinningSword.Size = UDim2.new(0, 133, 0, 39)
-SpinningSword.Font = Enum.Font.SourceSans
-SpinningSword.Text = "Spinning Sword"
-SpinningSword.TextColor3 = Color3.fromRGB(0, 0, 0)
-SpinningSword.TextSize = 14.000
-SpinningSword.MouseButton1Down:connect(function()
+UICorner_4.Parent = SwordLord
+
+SpinnerSword.Name = "Spinner Sword"
+SpinnerSword.Parent = Main
+SpinnerSword.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SpinnerSword.Position = UDim2.new(0.496062964, 0, 0.642045498, 0)
+SpinnerSword.Size = UDim2.new(0, 129, 0, 42)
+SpinnerSword.Font = Enum.Font.SourceSans
+SpinnerSword.Text = "Spinner (Joke)"
+SpinnerSword.TextColor3 = Color3.fromRGB(0, 0, 0)
+SpinnerSword.TextSize = 14.000
+SpinnerSword.MouseButton1Down:connect(function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Kixbiles/Kixbiles-Hub/main/sword%20spin.lua'))()
 end)
 
-FlingSpinner.Name = "Fling Spinner"
-FlingSpinner.Parent = Main
-FlingSpinner.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-FlingSpinner.Position = UDim2.new(0.458015263, 0, 0.315789461, 0)
-FlingSpinner.Size = UDim2.new(0, 156, 0, 35)
-FlingSpinner.Font = Enum.Font.SourceSans
-FlingSpinner.Text = "Revovld Sword (fling)"
-FlingSpinner.TextColor3 = Color3.fromRGB(0, 0, 0)
-FlingSpinner.TextSize = 14.000
-FlingSpinner.MouseButton1Down:connect(function()
+
+UICorner_5.Parent = SpinnerSword
+
+RevovlerSword.Name = "Revovler Sword"
+RevovlerSword.Parent = Main
+RevovlerSword.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RevovlerSword.Position = UDim2.new(0.496062994, 0, 0.261363626, 0)
+RevovlerSword.Size = UDim2.new(0, 129, 0, 42)
+RevovlerSword.Font = Enum.Font.SourceSans
+RevovlerSword.Text = "Fidget Spinner"
+RevovlerSword.TextColor3 = Color3.fromRGB(0, 0, 0)
+RevovlerSword.TextSize = 14.000
+RevovlerSword.MouseButton1Down:connect(function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Kixbiles/Kixbiles-Hub/main/fling%20spinner.lua'))()
 end)
 
-Hoverboard.Name = "Main Text"
-Hoverboard.Parent = Main
-Hoverboard.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Hoverboard.Position = UDim2.new(-0.000292751327, 0, -0.0044783745, 0)
-Hoverboard.Size = UDim2.new(0, 187, 0, 37)
-Hoverboard.Font = Enum.Font.SourceSans
-Hoverboard.Text = "Jays Sword GUI"
-Hoverboard.TextColor3 = Color3.fromRGB(0, 0, 0)
-Hoverboard.TextSize = 14.000
-
-FlingSpinner_2.Name = "Hoverboard"
-FlingSpinner_2.Parent = Main
-FlingSpinner_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-FlingSpinner_2.Position = UDim2.new(0.458015263, 0, 0.594736814, 0)
-FlingSpinner_2.Size = UDim2.new(0, 156, 0, 35)
-FlingSpinner_2.Font = Enum.Font.SourceSans
-FlingSpinner_2.Text = "HoverBoard (BETA)"
-FlingSpinner_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-FlingSpinner_2.TextSize = 14.000
-FlingSpinner_2.MouseButton1Down:connect(function()
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Kixbiles/Kixbiles-Hub/main/Hoverboard.lua'))()
-end)
+UICorner_6.Parent = RevovlerSword
